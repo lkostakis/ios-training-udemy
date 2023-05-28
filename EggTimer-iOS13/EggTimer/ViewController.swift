ee8cdd27b4d339e4757b082b86414defdafb77ed
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-05-28 14:27:07 +0300

    added timer and change title label

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -9,7 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
+    @IBOutlet weak var titleLabel: UILabel!
+    private enum HardnessTime: Int {
+        case softTime = 3
+        case mediumTime = 420
+        case hardTime = 720
+    }
+    private var timer: Timer?
+    private var secondsRemaining = 0
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+
+        titleLabel.text = "How do you like your eggs?"
+        stopTimer()
+
+        if buttonTitle == "Hard" {
+            secondsRemaining = HardnessTime.hardTime.rawValue
+        } else if buttonTitle == "Medium" {
+            secondsRemaining = HardnessTime.mediumTime.rawValue
+        } else {
+            secondsRemaining = HardnessTime.softTime.rawValue
+        }
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(updateTimer),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            stopTimer()
+            titleLabel.text = "Done!"
+        }
+    }
+
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
 }
