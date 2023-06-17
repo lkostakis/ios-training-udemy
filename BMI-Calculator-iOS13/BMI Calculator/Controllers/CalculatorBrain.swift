1c6277bd335fb05c7536359a765f615360f3d64b
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-06-17 16:51:10 +0300

    updated colors

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -16,26 +16,25 @@ struct CalculatorBrain {
         let bmiValue = weight / (height * height)
 
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor(red: 124/255,
-                                                                         green: 17/255,
-                                                                         blue: 255/255,
-                                                                         alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.4745098054,
+                                                                                      green: 0.8392156959,
+                                                                                      blue: 0.9764705896,
+                                                                                      alpha: 1))
         } else if bmiValue < 24.9 {
-            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor(red: 18/255,
-                                                                                  green: 211/255,
-                                                                                  blue: 77/255,
-                                                                                  alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644,
+                                                                                        green: 0.8862745166,
+                                                                                        blue: 0.5921568871,
+                                                                                        alpha: 1))
         } else {
-            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor(red: 190/255,
-                                                                                green: 0,
-                                                                                blue: 68/255,
-                                                                                alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.9098039269,
+                                                                                      green: 0.4784313738,
+                                                                                      blue: 0.6431372762,
+                                                                                      alpha: 1))
         }
     }
 
     func getBMIValue() -> String {
         String(format: "%.1f", bmi?.value ?? 0)
-
     }
 
     func getAdvice() -> String {
