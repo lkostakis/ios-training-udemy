73f51618bcc1ab504b77c3664d16ae45dae80257
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-07-02 18:00:40 +0300

    networking-- weather open API

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,60 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    
+    @IBOutlet weak var searchTextField: UITextField!
+
+    var weatherManager = WeatherManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        weatherManager.delegate = self
     }
 
-
+    @IBAction func searchTapped(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
 }
 
+// MARK: TextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Enter a location..."
+            return false
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+    }
+}
+
+// MARK: WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString.description
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+
+    func didFailWithError(error: Error) {
+
+    }
+}
---
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -0,0 +1,57 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Lefteris Kostakis on 2/7/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager {
+
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=de8e45b46c229cd13cec34fd9a90098f&units=metric"
+    var delegate: WeatherManagerDelegate?
+
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
+
+    func performRequest(with urlString: String) {
+
+        guard let url = URL(string: urlString) else {
+            return
+        }
+
+        let session = URLSession(configuration: .default)
+
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                delegate?.didFailWithError(error: error)
+                return
+            }
+
+            if let safeData = data {
+                let decoder = JSONDecoder()
+                do {
+                    let decodedData = try decoder.decode(WeatherData.self, from: safeData)
+                    let id = decodedData.weather[0].id
+                    let temp = decodedData.main.temp
+                    let name = decodedData.name
+
+                    let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+                    delegate?.didUpdateWeather(self, weather: weather)
+                } catch {
+                    delegate?.didFailWithError(error: error)
+                }
+            }
+        }
+        task.resume()
+    }
+}
